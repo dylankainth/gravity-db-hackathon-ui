53f76a143ed579b0b77bf68ca51d1bf54e0d5a1e
dylankainth <ykainth.dylan@gmail.com>
2023-07-13 23:10:35 +0100

    ui written

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,20 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+        rel: 'stylesheet',
+        integrity:
+          'sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM',
+        crossorigin: 'anonymous',
+      },
+    ],
+    script: [
+      {
+        src:"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js", integrity:"sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz", crossorigin:"anonymous"
+      }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -48,6 +61,7 @@ export default {
   build: {},
 
   server: {
-    port: 8080 // default: 3000
-  }
+    port: 8080, // default: 3000
+  },
+  ssr: false
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,67 @@
 <template>
-  <Tutorial />
+  <div class="container py-4">
+    <header class="pb-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center text-body-emphasis text-decoration-none">
+        <span class="fs-3 ml-2">IDEA 1.0</span>
+      </a>
+    </header>
+
+    <div class="p-5 mb-4 bg-body-tertiary rounded-3 shadow border">
+      <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold">Check financial documents.</h1>
+        <p class="col-md-8 fs-4">Scan your financial documents for *buzzword* in *special phrase*. Lorem ipsum, lorem ipsum dolor sit amet.</p>
+      </div>
+    </div>
+
+    <div class="row align-items-md-stretch">
+      <div class="col-md-6">
+        <div class="h-100 p-5 bg-body-tertiary rounded-3 shadow border">
+          <h2>Upload a file</h2>
+
+          <form action="/upload" method="post" enctype="multipart/form-data">
+            <input type="file" name="file" />
+            <button class="btn btn-primary" type="submit" value="Upload">Check</button>
+          </form>
+          <p><i>Supported types: pdf, xlsx, etc</i></p>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="h-100 p-5 bg-body-tertiary rounded-3 shadow border">
+          <h2>Results</h2>
+          
+
+          <table class="table rounded">
+            <thead>
+              <tr>
+                <th scope="col">India</th>
+                <th scope="col">Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>China</td>
+                <td>✅Passed</td>
+              </tr>
+              <tr>
+                <td>India</td>
+                <td>✅Passed</td>
+              </tr>
+              <tr>
+                <td>EU</td>
+                <td>❌Failed</td>
+              </tr>
+
+            </tbody>
+          </table>
+
+        </div>
+      </div>
+    </div>
+
+    <footer class="pt-3 mt-4 text-body-secondary border-top">
+      © 2023
+    </footer>
+  </div>
 </template>
 
 <script>
